3c298c533230db0f0c262d6494e6dcd915609dca
Gouspourd <gaspard.delpiano@gmail.com>
2022-01-07 13:27:56 +0100

    Update gun_list.lua

--- scripts/gun_list.lua
+++ scripts/gun_list.lua
@@ -3,10 +3,12 @@ require "scripts/gun"
 
 guns={
     --                  name       sprite        spd  cd     maxammo  ofdist    ofangle  
-    revolver = make_gun("revolver",spr_revolver, 30  ,.25    ,100      ,100     ,.1     ,
+    revolver = make_gun("revolver",spr_revolver, 300  ,.25    ,100      ,100     ,.1     ,
     -- shoot
     --function (g,p)
+        --return
         make_bullet--(g,p)
+        --return
     --end,
-),
+    ),
 }
