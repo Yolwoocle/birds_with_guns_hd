8d6425e6b0dae8b642d6d0714d0350b1889636ca
Gouspourd <gaspard.delpiano@gmail.com>
2022-01-26 19:42:39 +0100

    idk but yes

--- scripts/mob_list.lua
+++ scripts/mob_list.lua
@@ -20,36 +20,13 @@ mob_list = {
 		mv_mouvement = 3,
 		gun_dist = 14,
 		close_mv = false,
-		shoot_dist = 80,
+		shoot_dist = 150,
 		see_dist = 310,
 
 		closest_p = 70,
 		far_p	  = 80,
 	}),
 
-	knight = make_mob({
-		name = "knight",
-		spr = spr_fox[1],
-		life = 30,	
-		spd = 10,
-		w = 5,
-		h = 4,
-		hit_x = 13,
-		hit_y = 13,
-		gun = guns.knight_gun,
-
-		friction = 0.95,
-		bounce = 0.6,
-		mv_pause = 1.5,
-		mv_mouvement = 3,
-		gun_dist = 14,
-		close_mv = false,
-		shoot_dist = 1000,
-		see_dist = 310,
-
-		closest_p = 30,
-		far_p	  = 30,
-	}),
 
 	knight = make_mob({
 		name = "knight",
