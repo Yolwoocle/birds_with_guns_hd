90108a5a53f810922c0416d4b5e53e940e67cdc1
Yolwoocle <leo.bernard2015@hotmail.com>
2022-03-13 18:01:56 +0100

    Mobs + guns! BUFF

--- probability_tables/gun.lua
+++ probability_tables/gun.lua
@@ -1,9 +1,13 @@
 local distr = {
 	revolver = 1,
+	machinegun = 1,
+	minigun = 1,
 	laser = 1,
 	shotgun = 1,
 	assault_rifle = 1,
 	fire_extinguisher = 1,
+	firework_launcher = 1,
+	paper_plane_gun = 1,
 }
 
 return distr
--- probability_tables/mob_distribution.lua
+++ probability_tables/mob_distribution.lua
@@ -4,6 +4,7 @@ local distr = {
     fox = 20,
     robot = 3,
     cactus = 5,
+    shotgunboy = 4,
 }
 
 return distr
